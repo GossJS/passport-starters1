0d0ffece40a1391e7e3483fd932e3bb4323f3e91
gossoudarev <igossoudarev@herzen.spb.ru>
2017-12-04 04:59:59 +0300

    first

--- index.js
+++ index.js
@@ -1,150 +1,148 @@
-/*jshint esversion: 6 */
-const PORT = 4321;
 import express from 'express';
+import morgan from 'morgan';
 import path from 'path';
 import request from 'request';
 import url from 'url';
 import bodyParser from 'body-parser';
 import session from 'express-session';
 import passport from 'passport';
-import {Strategy as LocalStrategy} from 'passport-local';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as FBStrategy } from 'passport-facebook';
+import { ensureLoggedIn } from 'connect-ensure-login';
 
 import User from './bd/publicUsers';
 
-class Toy {
-	type="SOFT";
-	get getType(){
-		return this.type;
-	}
-}
+const PORT = 4321;
 
-const chebu = new Toy();
-
-
-const ensureAuth = (req, res, done)=>{
-	if (!req.isAuthenticated()) {
-		res.redirect('/login');
-	} else {
-	    //console.log(req.user); here we have like { _id: 599e171c2858b303814cac71,  username: 'teacher',  password: 'qq' }
-		return done();
-	}
+const ensureAuth = (req, res, done) => {
+  if (!req.isAuthenticated()) return res.redirect('/login');
+  // console.log(req.user);
+  // here we have  { _id: 599e171c2858b303814cac71,  username: 'teacher',  password: 'qq' }
+  return done();
 };
 
 
-
-	  
 const app = express();
 app
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
-  .use(bodyParser.urlencoded({
-      extended: true
-  }))
-      
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(morgan('combined'))
   .use((req, res, next) => next())
+;
 
-  ;
-
-passport.use(new LocalStrategy(
-  {
-      usernameField: 'login',
-      passwordField: 'pass'
-  },
-  (username, password, done)=>{ 
-	//нечто, что возвращает нужный вид done  
-	//это может быть любой мок-код, например, ищущий имя в JSON-файле и т.п.  
-	
-    User.findOne({ username },  (err, user)=>{
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (user.password !== password) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-	
-	
-  }
+passport.use(new FBStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: 'http://ikoder.xyz/auth/fb/done',
+},
+(accessToken, refreshToken, profile, done) => done(null, profile)
 ));
 
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
+/*
+// В продакшн,  Facebook-аккаунт должен быть
+// ассоциирован с записью о пользователе в БД приложения, что
+// обеспечивает связывание аккаунтов и аутентификацию при помощи
+// других провайдеров идентичности
+(accessToken, refreshToken, profile, done) =>
+  User.findOrCreate({ facebookId: profile.id },  (err, user) => done(err, user));
 
-passport.deserializeUser(function(_id, done) {
-  User.findById(_id, function(err, user) {
-    done(err, user);
+*/
+
+
+passport.use(new LocalStrategy({
+  usernameField: 'login',
+  passwordField: 'pass',
+},
+(username, password, done) => {
+// нечто, что возвращает нужный вид done
+// это может быть любой мок-код, например, ищущий имя в JSON-файле и т.п.
+
+  User.findOne({ username }, (err, user) => {
+    if (err) { return done(err); }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (user.password !== password) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
   });
-});
+}
+));
+
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser((_id, done) => User.findById(_id, (err, user) => done(err, user)));
 
 app
-  .use(session({ secret: 'mysecret', resave: true, saveUninitialized: true }  ))
+  .use(session({ secret: 'mysecret', resave: true, saveUninitialized: true }))
   .use(passport.initialize())
   .use(passport.session());
 
 
-app  
-  .get('/', (req, res) => {
-    res
-      .send('<h1>Welcome to Express!</h1><a href="/private">Private section</a>');
-  })
-
-  
-  .get('/private', ensureAuth,  (req, res) => {
-    res.set({
-        'Access-Control-Allow-Origin': '*',
-        'elias': 'goss'
-      })
-      .json({
-        'gossApi': 'protected!'
-      });
-  })
-
-  .get('/lalala', (r,res)=>res.send(chebu.getType))
-
-  .get('/bla', (r, res) => res.set({'refresh':'5', 'elias': 'go'}).sendFile(path.resolve(__dirname, 'public/1.txt')))
-
-  .get('/user/:username/:password',(req, res) => {
-       
-  			 const {username, password} = req.params;
-             User.findOne({ username },  (err, user)=>{
-               if (err) { throw new Error(err); }
-               if (!user) {
-                 return res.json( { message: 'Incorrect username.' });
-               }
-               if (user.password !== password) {
-                 return res.json( { message: 'Incorrect password.' });
-               }
-               return res.json( user);
-             });
-  
-  
-  })
-
-  
-  
-  .get('/login', (req, res) =>  request(url.format({protocol:'https',host:'kodaktor.ru',pathname: '/g/session_post'})).pipe(res))
-
-  .post('/login/check',
-      passport.authenticate('local', { successRedirect: '/',
-                                       failureRedirect: '/fail',
-                                     })
+app
+  .get('/', r => r.res.send(`
+          <h1>Welcome to Passport!</h1>
+          <div>
+            <a href="/localprivate">Private section</a> - protected by local /login <br>
+            <a href="/fbprivate">Private section</a> - protected by FB /auth <br>
+          </div>
+        `)
   )
 
-  .get('/logout', (req, res)=>{
+  .get('/localprivate', ensureAuth, r => r.res.send('<a href="/logout">Log out</a>'))
+// этот маршрут защищён локально
+// после первого успешного входа уже не потребуется авторизоваться заново
+
+  .get('/fbprivate', ensureLoggedIn(), r => r.res.send(`
+          <h1>Welcome to Profile!</h1>
+          <div>
+           ID: ${r.user.id} <br>
+           Display name: ${r.user.displayName} <br>
+           <a href="/logout">Log out</a>
+          </div>
+          `)
+  )
+// этот маршрут защищён фейсбуком
+// после первого успешного входа уже не потребуется авторизоваться заново
+
+  .get('/auth/fb',
+    passport.authenticate('facebook'))
+// фактически это значит показать форму, по аналогии с /login
+
+  .get('/auth/fb/done',
+    passport.authenticate('facebook', { failureRedirect: '/auth/fb' }),
+    (req, res) => res.redirect('/fbprivate')) // Successful authentication, redirect home.
+
+  .get('/user/:username/:password', (req, res) => {
+    // просто проверить, работает ли подключение к БД
+    const { username, password } = req.params;
+    User.findOne({ username }, (err, user) => {
+      if (err) { throw new Error(err); }
+      if (!user) {
+        return res.json({ message: 'Incorrect username.' });
+      }
+      if (user.password !== password) {
+        return res.json({ message: 'Incorrect password.' });
+      }
+      return res.json(user);
+    });
+  })
+
+
+  .get('/login', (req, res) => request(url.format({ protocol: 'https', host: 'kodaktor.ru', pathname: '/g/session_post' })).pipe(res))
+
+  .post('/login/check', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/fail' })
+  )
+
+  .get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
   })
 
-   
-  .get('/fail', () => {
-    throw new Error('Fail!');
-  })
-  .get('/my', (req, res) => res.send('MY'))
+
   .use((req, res) => res.send('404'))
   .use((err, req, res, next) => res.send(`500 ${err}`))
   .set('port', process.env.port || PORT)
   .listen(app.get('port'),
-    () => console.log(`­­> Port ${ app.get('port') } listening!!!`));
+    () => console.log(`­­> Port ${app.get('port')} listening!!!`));
