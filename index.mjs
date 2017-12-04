e2345274a6fc93e5e9b9cb134e88d801646b350b
root <igossoudarev@herzen.spb.ru>
2017-12-04 05:44:14 +0300

    first

--- index.mjs
+++ index.mjs
@@ -1,21 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
-import path from 'path';
 import request from 'request';
 import url from 'url';
 import bodyParser from 'body-parser';
 import session from 'express-session';
 import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-import { Strategy as FBStrategy } from 'passport-facebook';
-import { ensureLoggedIn } from 'connect-ensure-login';
+import passportLocal from 'passport-local';
+import passportFacebook from 'passport-facebook';
+import connectEnsureLogin from 'connect-ensure-login';
 
 import User from './bd/publicUsers';
 
-const PORT = 4321;
+const PORT = 80;
 
 const ensureAuth = (req, res, done) => {
-  if (!req.isAuthenticated()) return res.redirect('/login');
+  if (!req.isAuthenticated()) return res.redirect('/locallogin');
   // console.log(req.user);
   // here we have  { _id: 599e171c2858b303814cac71,  username: 'teacher',  password: 'qq' }
   return done();
@@ -24,14 +23,14 @@ const ensureAuth = (req, res, done) => {
 
 const app = express();
 app
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static('public'))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(morgan('combined'))
   .use((req, res, next) => next())
 ;
 
-passport.use(new FBStrategy({
+passport.use(new passportFacebook.Strategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   callbackURL: 'http://ikoder.xyz/auth/fb/done',
@@ -49,8 +48,8 @@ passport.use(new FBStrategy({
 
 */
 
-
-passport.use(new LocalStrategy({
+/*
+passport.use(new passportLocal.Strategy({
   usernameField: 'login',
   passwordField: 'pass',
 },
@@ -74,11 +73,15 @@ passport.use(new LocalStrategy({
 passport.serializeUser((user, done) => done(null, user._id));
 passport.deserializeUser((_id, done) => User.findById(_id, (err, user) => done(err, user)));
 
+*/
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
 app
   .use(session({ secret: 'mysecret', resave: true, saveUninitialized: true }))
   .use(passport.initialize())
   .use(passport.session());
-
 
 app
   .get('/', r => r.res.send(`
@@ -94,21 +97,21 @@ app
 // этот маршрут защищён локально
 // после первого успешного входа уже не потребуется авторизоваться заново
 
-  .get('/fbprivate', ensureLoggedIn(), r => r.res.send(`
+  .get('/fbprivate', connectEnsureLogin.ensureLoggedIn(), r => r.res.send(`
           <h1>Welcome to Profile!</h1>
           <div>
            ID: ${r.user.id} <br>
            Display name: ${r.user.displayName} <br>
-           <a href="/logout">Log out</a>
+           <a href="https://www.facebook.com/" target="_blank">Log out from facebook</a>
           </div>
           `)
   )
 // этот маршрут защищён фейсбуком
 // после первого успешного входа уже не потребуется авторизоваться заново
 
-  .get('/auth/fb',
+  .get(['/auth/fb', '/login'],
     passport.authenticate('facebook'))
-// фактически это значит показать форму, по аналогии с /login
+// фактически это значит показать форму
 
   .get('/auth/fb/done',
     passport.authenticate('facebook', { failureRedirect: '/auth/fb' }),
@@ -130,9 +133,9 @@ app
   })
 
 
-  .get('/login', (req, res) => request(url.format({ protocol: 'https', host: 'kodaktor.ru', pathname: '/g/session_post' })).pipe(res))
+  .get('/locallogin', (req, res) => request(url.format({ protocol: 'https', host: 'kodaktor.ru', pathname: '/g/localsession' })).pipe(res))
 
-  .post('/login/check', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/fail' })
+  .post('/locallogin/check', passport.authenticate('local', { successRedirect: '/localprivate', failureRedirect: '/' })
   )
 
   .get('/logout', (req, res) => {
