6e1be4ba5fe468a9dd8a45348b8d34e744932cd6
Ilya Goss <igossoudarev@herzen.spb.ru>
2017-12-04 06:50:15 +0300

    Create publicUsers.mjs

---
+++ bd/publicUsers.mjs
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    "username": {
+      "type": "String"
+    },
+    "password": {
+      "type": "String"
+    }},
+    {"collection": "userlist"}
+),
+  User = mongoose.model(null, UserSchema);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://userreader:Qwerty.123@kodaktor.ru/users', {}, err => {
+  if (err) throw err;
+});
+
+export default User;
